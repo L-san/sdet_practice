121678b5b555890a9131f53a26e2c3511722f4bb
L-san <lsarelli9402@gmail.com>
2023-03-30 22:36:40 +0400

    some configs for chrome

--- src/test/java/utils/WebDriverFactory.java
+++ src/test/java/utils/WebDriverFactory.java
@@ -10,6 +10,9 @@ public class WebDriverFactory {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--start-maximized");
         chromeOptions.addArguments("--remote-allow-origins=*");
+        chromeOptions.addArguments("--no-sandbox");
+        chromeOptions.addArguments("--headless"); //!!!should be enabled for Jenkins
+        chromeOptions.addArguments("--disable-dev-shm-usage"); //!!!should be enabled for Jenkins
         return new ChromeDriver(chromeOptions);
     }
 }
