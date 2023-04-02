0f7c4598e5111b12f897349e841b165d9f2f8420
L-san <lsarelli9402@gmail.com>
2023-04-02 21:30:10 +0400

    Update WebDriverFactory.java

--- src/test/java/utils/WebDriverFactory.java
+++ src/test/java/utils/WebDriverFactory.java
@@ -7,10 +7,10 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 public class WebDriverFactory {
     public static ChromeDriver getChromeDriver(){
-        ChromeDriverManager.getInstance().setup("111");
+        ChromeDriverManager.getInstance().setup("110.0.5452.0");
         ChromeOptions chromeOptions = new ChromeOptions();
         //chromeOptions.addArguments("--start-maximized");
-        // chromeOptions.addArguments("--remote-allow-origins=*");
+        chromeOptions.addArguments("--remote-allow-origins=*");
         chromeOptions.addArguments("--no-sandbox");
         chromeOptions.addArguments("--headless"); //!!!should be enabled for Jenkins
         chromeOptions.addArguments("--disable-dev-shm-usage");
