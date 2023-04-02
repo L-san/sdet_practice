df325c4b998b904ed0af5a189f7e969256f582d1
L-san <lsarelli9402@gmail.com>
2023-04-02 14:44:34 +0400

    for ci

--- src/test/java/utils/WebDriverFactory.java
+++ src/test/java/utils/WebDriverFactory.java
@@ -1,6 +1,7 @@
 package utils;
 
 import io.github.bonigarcia.wdm.ChromeDriverManager;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -8,11 +9,11 @@ public class WebDriverFactory {
     public static ChromeDriver getChromeDriver(){
         ChromeDriverManager.getInstance().setup("111");
         ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--start-maximized");
-        chromeOptions.addArguments("--remote-allow-origins=*");
+        //chromeOptions.addArguments("--start-maximized");
+        // chromeOptions.addArguments("--remote-allow-origins=*");
         chromeOptions.addArguments("--no-sandbox");
         chromeOptions.addArguments("--headless"); //!!!should be enabled for Jenkins
-        chromeOptions.addArguments("--disable-dev-shm-usage"); //!!!should be enabled for Jenkins
+        chromeOptions.addArguments("--disable-dev-shm-usage");
         return new ChromeDriver(chromeOptions);
     }
 }
